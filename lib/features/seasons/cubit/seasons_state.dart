891655e9bd566b1e46e989a9a9cfe641b6d5cf12
Levi <phamnguyentanphucgl@gmail.com>
2023-10-26 15:54:59 +0700

    feature section

---
+++ lib/features/seasons/cubit/seasons_state.dart
@@ -0,0 +1,10 @@
+part of 'seasons_bloc.dart';
+
+class SeasonsState extends Equatable {
+  const SeasonsState();
+
+  @override
+  List<Object?> get props => [];
+
+  SeasonsState copyWith() => const SeasonsState();
+}
