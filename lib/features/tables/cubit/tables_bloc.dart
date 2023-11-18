31ec12f6f8d9bbf21b12089bb41642b8669ef5b5
Levi <phamnguyentanphucgl@gmail.com>
2023-11-18 13:21:47 +0700

    api tables

--- lib/features/tables/cubit/tables_bloc.dart
+++ lib/features/tables/cubit/tables_bloc.dart
@@ -1,8 +1,22 @@
+import 'package:app/network/model/table_model.dart';
+import 'package:app/network/repositories/domain.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 
 part 'tables_state.dart';
 
-class ScheduleBloc extends Cubit<ScheduleState> {
-  ScheduleBloc() : super(ScheduleState());
+class TableBloc extends Cubit<TableState> {
+  TableBloc() : super(TableState(data: TableModel())) {
+    getTableModel();
+  }
+
+  Domain get _domain => GetIt.I<Domain>();
+
+  Future<void> getTableModel() async {
+    final result = await _domain.repo.getTableModel();
+    if (result.isSuccess) {
+      emit(state.copyWith(data: result.data));
+    } else {}
+  }
 }
--- lib/features/tables/cubit/tables_state.dart
+++ lib/features/tables/cubit/tables_state.dart
@@ -1,7 +1,21 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'tables_bloc.dart';
 
-class ScheduleState extends Equatable {
+class TableState extends Equatable {
+  final TableModel data;
+  TableState({
+    required this.data,
+  });
   @override
-  List<Object?> get props => throw UnimplementedError();
+  List<Object?> get props => [
+        data,
+      ];
+
+  TableState copyWith({
+    TableModel? data,
+  }) {
+    return TableState(
+      data: data ?? this.data,
+    );
+  }
 }
